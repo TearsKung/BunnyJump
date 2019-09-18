937812d53ef4dbdae528af9835705591f3ae4d86
Sunny <sunza987@hotmail.co.th>
2019-09-18 16:46:31 +0700

    upload

---
+++ Player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+var motion = Vector2(0,0)
+
+export var SPEED = 500 
+export var Gravity = 500
+# warning-ignore:unused_argument
+func _physics_process(delta):
+	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+		motion.x = -SPEED
+		print("fuck")
+	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
+		motion.x = SPEED
+	elif Input.is_action_pressed("jump"):
+		motion.y = -Gravity
+	else:
+		motion.x = 0
+	if Input.is_action_pressed("jump"):
+		motion.y = -Gravity
+	else:
+		motion.y = Gravity
+	
+	
---
+++ Stand/Sand Land.gd
@@ -0,0 +1,6 @@
+extends KinematicBody2D
+export var motion = Vector2()
+export var cycle = 1.0
+var accum = 0.0
+
+
